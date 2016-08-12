b3b62bdd4e548d8d7c1a4e81d7f32553359f6194
Miro382 <miro382@centrum.sk>
2016-08-12 21:35:37 +0200

    Add files via upload

--- Examples/WeatherApplication/Form1.Designer.cs
+++ Examples/WeatherApplication/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -130,11 +132,20 @@
             this.label10.TabIndex = 9;
             this.label10.Text = "-";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 78);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -148,6 +159,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,6 +177,7 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Examples/WeatherApplication/Form1.cs
+++ Examples/WeatherApplication/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using CSharp_Weather;
 using System;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace WeatherApplication
@@ -13,9 +14,10 @@ namespace WeatherApplication
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Weather weather = new Weather("1234567891011121345", Weather.Celsius);
+            /*
+            Weather weather = new Weather("79f4bd16a8425b5ba8153d93e67a7c21", Weather.Fahrenheit);
             weather.SetLanguage("en",true);
-            if(weather.GetWeather("London"))
+            if(weather.GetWeather(weather.GetCountry()))
             {
                 if(weather.GetWeatherIsSuccessful())
                 {
@@ -26,6 +28,26 @@ namespace WeatherApplication
                     label10.Text = weather.weatherinfo.CityName + "  ( " + weather.weatherinfo.CountryCode+" )";
                 }
             }
+            */
+
+            
+            CityLocator cityloc = new CityLocator();
+            cityloc.GetGeoCoordByCityName("Michalovce");
+
+            WeatherMET weather = new WeatherMET();
+            if(weather.GetWeatherData(cityloc.Latitude,cityloc.Longtitude))
+            {
+                pictureBox1.Image = weather.GetIcon(0);
+
+                label2.Text = weather.weatherinfo.Temperature + "°C";
+                label4.Text = weather.weatherinfo.Humidity + " " + weather.weatherinfo.HumidityUnit;
+                label6.Text = weather.weatherinfo.WindSpeed;
+                label8.Text = weather.weatherinfo.Pressure + " " + weather.weatherinfo.PressureUnit;
+                label10.Text = cityloc.PlaceDisplayName;
+            }
+            
+
+
         }
 
 
